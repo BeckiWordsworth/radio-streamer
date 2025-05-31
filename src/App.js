65e0c63fcab1cee67b336254bf9b747f0be34600
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2025-05-31 21:59:04 +0200

    First Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="headerBox">
+        <h1>Sverige Radio Stations</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
